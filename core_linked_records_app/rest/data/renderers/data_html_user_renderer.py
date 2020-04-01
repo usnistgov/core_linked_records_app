d584e8fcf36b543869e65b8674f581a5c265b835
Philippe Dessauw <philippe.dessauw@nist.gov>
2020-04-01 11:11:05 -0400

    fix: improve HTML 404 for PID not found
    Change-Id: Ib9e62a6b01b0cd5ff7b076e97e656b8eb77e0679

--- core_linked_records_app/rest/data/renderers/data_html_user_renderer.py
+++ core_linked_records_app/rest/data/renderers/data_html_user_renderer.py
@@ -1,5 +1,6 @@
 """ Data HTML renderer for django REST API
 """
+import logging
 from django.http import HttpResponse
 from rest_framework import renderers
 from rest_framework.status import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
@@ -7,24 +8,18 @@ from rest_framework.status import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_E
 from core_main_app.components.data import api as data_api
 from core_main_app.utils.rendering import render
 
+LOGGER = logging.getLogger(__name__)
+
 
 class DataHtmlUserRenderer(renderers.BaseRenderer):
     media_type = 'text/html'
     format = 'html'
     charset = 'utf-8'
 
-    def render(self, data, media_type=None, renderer_context=None):
-        """ Render the data object by returning the user template
-
-        Args:
-            data:
-            media_type:
-            renderer_context:
-
-        Returns: html page
-        """
+    @staticmethod
+    def build_page(data, request):
         context = {
-            'data': data_api.get_by_id(data["id"], renderer_context["request"].user)
+            'data': data_api.get_by_id(data["id"], request.user)
         }
 
         assets = {
@@ -49,7 +44,34 @@ class DataHtmlUserRenderer(renderers.BaseRenderer):
                 return HttpResponse(status=HTTP_500_INTERNAL_SERVER_ERROR)
         else:
             return render(
-                renderer_context['request'],
-                'core_main_app/user/data/detail.html', context=context,
+                request, 'core_main_app/user/data/detail.html', context=context,
                 assets=assets
+            )
+
+    def render(self, data, media_type=None, renderer_context=None):
+        """ Render the data object by returning the user template
+
+        Args:
+            data:
+            media_type:
+            renderer_context:
+
+        Returns: html page
+        """
+        try:
+            return self.build_page(data, renderer_context["request"])
+        except Exception as e:
+            LOGGER.error("Error while building data page: %s" % str(e))
+
+            if "kwargs" in renderer_context and "record" in renderer_context["kwargs"]:
+                error_msg = "Document %s does not exist." % \
+                            renderer_context["kwargs"]["record"]
+            else:
+                error_msg = "Invalid request provided."
+
+            return render(
+                renderer_context["request"], 'core_main_app/common/commons/error.html',
+                context={
+                    "error": error_msg
+                }
             )
--- core_linked_records_app/rest/urls.py
+++ core_linked_records_app/rest/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import url
 from core_linked_records_app.rest.providers import views as providers_views
 
 urlpatterns = [
-    url(r'^(?P<provider>[^/]+)/(?P<record>.+)$',
+    url(r'^(?P<provider>[^/]+)/(?P<record>.*)$',
         providers_views.ProviderRecord.as_view(),
         name='core_linked_records_app_rest_provider_record_view')
 ]
