71cbb3b453b109ca7f9c3fe15330990a865d45fa
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2020-09-01 08:44:00 -0400

    docs: add settings doc
    Change-Id: Ic848dedccfc86d85804886390a4b0f19bb23fbbf

--- core_linked_records_app/settings.py
+++ core_linked_records_app/settings.py
@@ -1,4 +1,7 @@
 """ Core linked records app settings
+
+Settings with the following syntax can be overwritten at the project level:
+SETTING_NAME = getattr(settings, "SETTING_NAME", "Default Value")
 """
 from django.conf import settings
 
