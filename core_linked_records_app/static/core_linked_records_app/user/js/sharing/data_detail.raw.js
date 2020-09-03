2ddf0a63c84ed7d8391dc6fb620efc93b6ae477b
Philippe Dessauw <philippe.dessauw@nist.gov>
2020-09-03 15:04:06 -0400

    fix: double definition of dataId in JS
    Change-Id: I50cda0e45a71c1b0a29488a46ea3f5fcfb395546

--- core_linked_records_app/static/core_linked_records_app/user/js/sharing/data_detail.raw.js
+++ core_linked_records_app/static/core_linked_records_app/user/js/sharing/data_detail.raw.js
@@ -1,2 +1,2 @@
-let dataId = "{{ data.data.id }}";
-let retrieveDataPidUrl = "{% url 'core_linked_record_retrieve_data_pid_url' %}";
+var dataId = dataId || "{{ data.data.id }}";
+var retrieveDataPidUrl = "{% url 'core_linked_record_retrieve_data_pid_url' %}";
