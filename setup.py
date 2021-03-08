3e0ccb72e0c35e1263e1fa5eef80f5b6cf8c0417
Guillaume Sousa <guillaume.sousa@nist.gov>
2021-03-08 16:07:28 +0000

    chore: upgrade 1.0.0-beta6
    Change-Id: Ib8f626c370022d6d63d06d1afd4c5516b8109764

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_linked_records_app",
-    version="1.0.0-beta5",
+    version="1.0.0-beta6",
     description="Linked records for the core project",
     long_description=long_desc,
     author="NIST IT Lab",
