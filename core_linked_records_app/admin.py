0321a685b07069e3322acef7dc2b5279eb849e8e
Guillaume Sousa <guillaume.sousa@nist.gov>
2022-09-19 16:24:00 -0400

    fix: prevent admin from adding or deleting pid settings
    Change-Id: Iaea903895421a758c0f37b44f4c0ecbb50fb69f3

--- core_linked_records_app/admin.py
+++ core_linked_records_app/admin.py
@@ -5,10 +5,13 @@ from django.contrib import admin
 
 from core_linked_records_app.components.local_id.admin_site import CustomLocalIdAdmin
 from core_linked_records_app.components.local_id.models import LocalId
+from core_linked_records_app.components.pid_settings.admin_site import (
+    CustomPidSettingsAdmin,
+)
 from core_linked_records_app.components.pid_settings.models import PidSettings
 from core_linked_records_app.components.pid_xpath.admin_site import CustomPidXpathAdmin
 from core_linked_records_app.components.pid_xpath.models import PidXpath
 
 admin.site.register(LocalId, CustomLocalIdAdmin)
-admin.site.register(PidSettings)
+admin.site.register(PidSettings, CustomPidSettingsAdmin)
 admin.site.register(PidXpath, CustomPidXpathAdmin)
---
+++ core_linked_records_app/components/pid_settings/admin_site.py
@@ -0,0 +1,15 @@
+""" Custom admin site for the PidSettings model
+"""
+from django.contrib import admin
+
+
+class CustomPidSettingsAdmin(admin.ModelAdmin):
+    """CustomPidSettingsAdmin"""
+
+    def has_add_permission(self, request, obj=None):
+        """Prevent from manually adding PidSettings"""
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        """Prevent from manually deleting PidSettings"""
+        return False
