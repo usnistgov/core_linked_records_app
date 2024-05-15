479a776f0be1063ee0217bfe544d9bdbc145a84d
Guillaume Sousa <guillaume.sousa@nist.gov>
2024-05-15 13:29:15 +0000

    chore: upgrade 2.10.0
    Change-Id: I2a24e2c2ca72b88ac04a325efef837ca60ffefa2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_linked_records_app",
-    version="2.9.0",
+    version="2.10.0",
     description="Linked records for the core project",
     long_description=long_desc,
     author="NIST IT Lab",
